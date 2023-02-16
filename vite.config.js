eeb13934d76e884641012bac3338b14454f50d65
ssozr <a917382460@gmail.com>
2023-02-17 00:55:45 +0800

    更新2

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
     })
   ],
-  base: '/VueWeek6/',
+  base: '/VueWeek62/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
